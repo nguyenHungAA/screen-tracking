aebc300c56e306e774313dda16175bfb9b79101a
nguyenhungAAA <quanghunglenguyen@gmail.com>
2025-12-17 10:06:35 +0700

    fix: java package name and xml content mismatch

--- www/ScreenTracking.js
+++ www/ScreenTracking.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'ScreenTracking', 'coolMethod', [arg0]);
+exports.coolMethod = function (success, error) {
+    exec(success, error, 'ScreenTracking', 'coolMethod', []);
 };
