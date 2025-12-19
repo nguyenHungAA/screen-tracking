7bf990de5bf9217ca2ff3c56cb94f2e1976def11
nguyenhungAAA <quanghunglenguyen@gmail.com>
2025-12-19 15:26:20 +0700

    feat: test permission request with camera function

---
+++ src/android/PermissionHelper.java
@@ -0,0 +1,10 @@
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class PermissionHelper {
+
+}
--- www/ScreenTracking.js
+++ www/ScreenTracking.js
@@ -1,5 +1,16 @@
 var exec = require('cordova/exec');
+var permissions = cordova.plugins.permissions;
 
 exports.coolMethod = function (success, error) {
     exec(success, error, 'ScreenTracking', 'coolMethod', ['hello i am running']);
 };
+
+permissions.requestPermission(permissions.CAMERA, success, error);
+
+function error() {
+    console.warn('Camera permission is not turned on');
+}
+
+function success(status) {
+    if (!status.hasPermission) error();
+}
